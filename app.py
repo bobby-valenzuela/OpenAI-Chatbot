34d5de06c5176959db65a1b0ee9b2e995d092a7b
Bobby Valenzuela <bobby.valenzuela@protonmail.com>
2023-04-27 23:23:37 -0700

    storing chat history

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
+#!/usr/bin/env python3
 import openai
 from dotenv import dotenv_values
 
 config = dotenv_values('.env');
 openai.api_key = config["OPENAI_API_KEY"]
 
-response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        { "role": "user", "content": "tell me a joke" }
-    ]
-)
+messages = []
 
-result = response.choices[0].message.content
+while True:
 
-print(result)
+    try:
+        usr_input = input(str("You: "))
+        message = { "role": "user", "content": usr_input }
+        # Store our latest input into chat history
+        messages.append(message)
+
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages
+        )
+        # Store reply into chat history
+        messages.append(response.choices[0]["message"].to_dict())
+        reply = response.choices[0].message.content
+
+        print(f"Assistant: {reply}")
+
+    except KeyboardInterrupt:
+        print("Exiting...")
+        break
+
